cb79dd0b440583700ccdd7f152f5cd3a0c32987a
Beatzoid <epiccoder7@gmail.com>
2020-12-01 19:30:32 -0800

    ✨ Add Upvote/Downvote to UI
    Add the ability to upvote/downvote posts on the UI

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,10 +2,18 @@ import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
 import { Layout } from "../components/Layout";
-import { Box, Flex, Heading, Link, Stack, Text } from "@chakra-ui/layout";
 import NextLink from "next/link";
-import { Button } from "@chakra-ui/button";
+import {
+    Box,
+    Flex,
+    Heading,
+    Link,
+    Stack,
+    Text,
+    Button
+} from "@chakra-ui/react";
 import { useState } from "react";
+import { UpdootSection } from "../components/UpdootSection";
 
 const Index = () => {
     const [variables, setVariables] = useState({
@@ -41,11 +49,21 @@ const Index = () => {
             ) : (
                 <Stack spacing={8}>
                     {data!.posts.posts.map((p) => (
-                        <Box key={p.id} p={5} shadow="md" borderWidth="1px">
-                            <Heading fontSize="xl">{p.title}</Heading>{" "}
-                            <Text>Posted by: {p.creator.username}</Text>
-                            <Text mt={4}>{p.textSnippet}</Text>
-                        </Box>
+                        <>
+                            <Flex
+                                key={p.id}
+                                p={5}
+                                shadow="md"
+                                borderWidth="1px"
+                            >
+                                <UpdootSection post={p} />
+                                <Box>
+                                    <Heading fontSize="xl">{p.title}</Heading>
+                                    <Text>Posted by: {p.creator.username}</Text>
+                                    <Text mt={4}>{p.textSnippet}</Text>
+                                </Box>
+                            </Flex>
+                        </>
                     ))}
                 </Stack>
             )}
