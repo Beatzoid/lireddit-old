54094d8746f1480645aacf79b40bf658c6dc152e
Beatzoid <epiccoder7@gmail.com>
2021-02-07 09:33:46 -0800

    ✨ Setup MikroORM
    Setup MikroORM with a basic entity and migration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+import { MikroORM } from "@mikro-orm/core";
+import { Post } from "./entities/Post";
+import { __prod__ } from "./utils/constants";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+    orm.getMigrator().up();
+
+    // const post = orm.em.create(Post, { title: "First post!" });
+    // await orm.em.persistAndFlush(post);
+
+    const posts = await orm.em.find(Post, {});
+    console.log(posts);
+};
+
+main().catch((err) => console.error(err));
---
+++ server/src/mikro-orm.config.ts
@@ -0,0 +1,20 @@
+import { Post } from "./entities/Post";
+import { __prod__ } from "./utils/constants";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+
+export default {
+    migrations: {
+        path: path.join(__dirname, "./migrations"),
+        pattern: /^[\w-]+\d+\.[tj]s$/
+    },
+    entities: [Post],
+    dbName: "lireddit",
+    type: "postgresql",
+    // You will need to change these details
+    // They will not work for you as
+    // This database is hosted locally
+    user: "beatzoid",
+    password: "Beatzoid!@#",
+    debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0];
