2646b737dd7dedd457aecc168c5470651ec9109c
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-04-13 12:30:09 -0700

    ✨ Setup Apollo and Resolvers
    Setup the Apollo graphql server and make a basic hello resolver

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,32 @@
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+
 import { __PROD__ } from "./constants";
 import microConfig from "./mikro-orm.config";
+
+// Resolvers
+import { HelloResolver } from "./resolvers/Hello";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
 
-    // const post = orm.em.create(Post, { title: "My first post" });
-    // await orm.em.persistAndFlush(post);
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () =>
+        console.log("Server listening on http://localhost:4000")
+    );
 };
 
 main().catch((err) => console.error(err));
