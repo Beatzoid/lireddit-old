067fb7b3d733e40c3b0ba575610efcbdb3da7d43
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-04-12 20:09:55 -0700

    ✨ Setup MikroORM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+import { MikroORM } from "@mikro-orm/core";
+import { __PROD__ } from "./constants";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
+
+    // const post = orm.em.create(Post, { title: "My first post" });
+    // await orm.em.persistAndFlush(post);
+};
+
+main().catch((err) => console.error(err));
---
+++ server/src/mikro-orm.config.ts
@@ -0,0 +1,17 @@
+import { __PROD__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+
+export default {
+    migrations: {
+        path: path.join(__dirname, "./migrations"),
+        pattern: /^[\w-]+\d+\.[tj]s$/
+    },
+    entities: [Post],
+    dbName: "lireddit",
+    type: "postgresql",
+    user: "beatzoid",
+    password: "beatzoid",
+    debug: !__PROD__
+} as Parameters<typeof MikroORM.init>[0];
