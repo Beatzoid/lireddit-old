513dc32db61647afe635d84b0049521e6c9aca6b
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-04-17 08:48:01 -0700

    ✨ Add Permissions to User Emails

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -2,10 +2,12 @@ import {
     Arg,
     Ctx,
     Field,
+    FieldResolver,
     Mutation,
     ObjectType,
     Query,
-    Resolver
+    Resolver,
+    Root
 } from "type-graphql";
 import argon2 from "argon2";
 import { v4 } from "uuid";
@@ -35,8 +37,19 @@ class UserResponse {
     user?: User;
 }
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
+    @FieldResolver(() => String)
+    email(@Root() user: User, @Ctx() { req }: MyContext) {
+        // This is the current user and it's ok to show them their own email
+        if (req.session.userId === user.id) {
+            return user.email;
+        }
+
+        // The current user wants to see someone elses email
+        return "";
+    }
+
     @Mutation(() => UserResponse)
     async changePassword(
         @Arg("token") token: string,
